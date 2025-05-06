e8b93e42c4f5beea2f9c6635dae6d59c8dfaa04a
AssTrahanec <max197402@gmail.com>
2025-05-06 16:06:45 +0300

    fix: changes

--- internal/migrations/20250506103845_init_schema.sql
+++ internal/migrations/20250506103845_init_schema.sql
@@ -15,24 +15,20 @@ CREATE INDEX idx_books_genre ON books(genre);
 CREATE TABLE users
 (
     user_id       UUID PRIMARY KEY,
-    role          VARCHAR(255) NOT NULL,
-    login         VARCHAR(255) NOT NULL,
+    role          VARCHAR(255) NOT NULL DEFAULT 'user',
+    login         VARCHAR(255) NOT NULL UNIQUE,
     password_hash varchar(255) NOT NULL
 );
 CREATE TABLE users_books
 (
-    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-    book_id UUID NOT NULL REFERENCES books(book_id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    book_id UUID REFERENCES books(book_id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, book_id)
 );
 
 CREATE INDEX idx_users_books_book_id ON users_books(book_id);
 
 -- +goose Down
-DROP INDEX IF EXISTS idx_books_author;
-DROP INDEX IF EXISTS idx_books_publication_year;
-DROP INDEX IF EXISTS idx_books_genre;
-DROP INDEX IF EXISTS idx_users_books_book_id;
 DROP TABLE IF EXISTS users_books;
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
