f700fa6dddaf5470ba440297edf13508f55c47eb
AssTrahanec <max197402@gmail.com>
2025-05-09 20:10:45 +0300

    added config load

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,11 @@
-package cmd
+package main
+
+import (
+	"bookService/config"
+	"fmt"
+)
 
 func main() {
-
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 }
---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env  string     `yaml:"env" env-default:"local"`
+	GRPC GRPCConfig `yaml:"grpc"`
+	DB   DBConfig   `yaml:"db"`
+}
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+type DBConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	DBName   string `yaml:"dbname"`
+	SSLMode  string `yaml:"sslmode"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config path does not exist")
+	}
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+	flag.StringVar(&res, "config", "", "config file path")
+	flag.Parse()
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
